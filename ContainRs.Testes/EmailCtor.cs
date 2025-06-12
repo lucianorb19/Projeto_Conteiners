340dac1b4a7bd17b00f2590a8d04fe1f9f5502d0
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-06-12 14:19:12 -0300

    xUnit Test for Email

---
+++ ContainRs.Testes/EmailCtor.cs
@@ -0,0 +1,20 @@
+using ContainRs.WebApp.Models;
+using System.Reflection;
+
+namespace ContainRs.Testes
+{
+    public class EmailCtor
+    {
+        [Fact]
+        public void Deve_Lancar_ArgumentException_Quando_Valor_Invalido()
+        {
+            // arrange
+            string emailInvalido = "valor qualquer";
+
+            // act & assert
+            Assert.Throws<ArgumentException>(() => new Email(emailInvalido));
+            //AVALIA SE É LANÇADA UMA EXCEÇÃO DO TIPO ArgumentException QUANDO É
+            //TENTADO CRIAR UM OBJETO Email A PARTIR DE UMA STRING "valor qualquer"
+        }
+    }
+}
